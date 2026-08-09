efa0064d2b9aac93b61d5d8b09150ae6c4047702
Jazz Jeremy Guico <jazzguico1@gmail.com>
2026-08-10 01:08:44 +0800

    Refactor deposit and withdrawal handling in Main

--- Main.java
+++ Main.java
@@ -5,20 +5,39 @@ public class Main {
         // user Jazz
         BankAccount myBankAccount = new BankAccount("Jazz");
         // deposit
-        myBankAccount.deposit(100);
+        boolean check = myBankAccount.deposit(100);
+        if (!check) {
+            System.out.println("Deposit failed.");
+        } else {
+            System.out.println("Deposit succeeded.");
+        }
+
         System.out.println("User: " + myBankAccount.getAccountHolder() + " deposited an amount of: " + myBankAccount.getBalance());
+
         // withdraw
-        myBankAccount.withdraw(50);
+        boolean success = myBankAccount.withdraw(50);
+        if (!success) {
+            System.out.println("Withdrawal denied: Insufficient funds");
+        } else {
+            System.out.println("Withdrawal successful.");
+        }
         System.out.println("User: " + myBankAccount.getAccountHolder() + " balance after withdrawal: " + myBankAccount.getBalance());
+
+
 
         // user Ysabell
         BankAccount myOtherBankAccount = new BankAccount("Ysabell");
         // deposit
         myOtherBankAccount.deposit(100);
+        if (!check) {
+            System.out.println("Deposit failed.");
+        } else {
+            System.out.println("Deposit succeeded.");
+        }
         System.out.println("User: " + myOtherBankAccount.getAccountHolder() + " deposited an amount of: " + myOtherBankAccount.getBalance());
-        // withdraw
 
-        boolean success = myOtherBankAccount.withdraw(200); // "success" is a local variable, usable only in this method. 
+        // withdraw
+        success = myOtherBankAccount.withdraw(-200); 
         if (!success) {
             System.out.println("Withdrawal denied: Insufficient funds");
         } else {
