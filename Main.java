a73f9d52eae59f28b0d67ea7c09ee2ea2d2fec3b
Jazz Jeremy Guico <jazzguico1@gmail.com>
2026-08-11 03:50:35 +0800

    Enhance Main class to include statement generation
    Added functionality to generate statements for accounts.

--- Main.java
+++ Main.java
@@ -15,5 +15,15 @@ public class Main {
             boolean result = acc.withdraw(98); // pick an amount where the fee matters
             System.out.println(acc.getAccountHolder() + " withdrawal result: " + result + ", balance: " + acc.getBalance());
         }
+
+        List<Statementable> statementAccounts = new ArrayList<>();
+        statementAccounts.add(new BankAccount("Jazz"));
+        statementAccounts.add(new SavingsAccount("Ysabell", 0.02));
+        statementAccounts.add(new BusinessAccount("Miguel", 5));
+        statementAccounts.add(new LoanAccount("Alice", 1000));
+
+        for (Statementable acc : statementAccounts) {
+            System.out.println(acc.generateStatement());
+        }
     }
 }
