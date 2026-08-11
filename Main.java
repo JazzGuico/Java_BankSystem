8576419f5446dc18398df42cbd932eb2625b621c
Jazz Jeremy Guico <jazzguico1@gmail.com>
2026-08-12 02:29:38 +0800

    Change accounts storage from List to Map
    Refactored account storage from List to Map for better retrieval.

--- Main.java
+++ Main.java
@@ -2,19 +2,18 @@ package Bank;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        List<BankAccount> accounts = new ArrayList<>();
-        accounts.add(new BankAccount("Jazz"));
-        accounts.add(new SavingsAccount("Ysabell", 0.02));
-        accounts.add(new BusinessAccount("Miguel", 5));
+        Map<String, BankAccount> accounts = new HashMap<>();
+        accounts.put("Jazz", new BankAccount("Jazz"));
+        accounts.put("Ysabell", new SavingsAccount("Ysabell", 0.02));
+        accounts.put("Miguel", new BusinessAccount("Miguel", 5));
 
-        for (BankAccount acc : accounts) {
-            acc.deposit(100);
-            boolean result = acc.withdraw(98); // pick an amount where the fee matters
-            System.out.println(acc.getAccountHolder() + " withdrawal result: " + result + ", balance: " + acc.getBalance());
-        }
+        BankAccount miguelAccount = accounts.get("Miguel"); // retrieves the BankAccount object associated with the key "Miguel" from the accounts map
+        System.out.println(miguelAccount.getAccountHolder() + " balance: " + miguelAccount.getBalance()); // Output: Miguel balance: 0.0
 
         List<Statementable> statementAccounts = new ArrayList<>();
         statementAccounts.add(new BankAccount("Jazz"));
