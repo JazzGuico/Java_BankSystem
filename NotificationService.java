4256068fef8dcf00d248faada90c5c819324ba8f
Jazz Jeremy Guico <jazzguico1@gmail.com>
2026-08-13 02:37:11 +0800

    Add NotificationService interface for notifications

---
+++ NotificationService.java
@@ -0,0 +1,5 @@
+package Bank;
+
+public interface NotificationService {
+    void sendNotification(String accountHolder, String message); // method to send a notification to the account holder with a message
+}
